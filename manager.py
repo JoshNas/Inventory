fbfb4f1507489113bc7c5c5adda88212262e55a1
JoshNas <36458322+JoshNas@users.noreply.github.com>
2018-09-18 19:18:11 -0400

    Update 9/18
    Added error checking for adding items
    Added scrollbar to display window

--- manager.py
+++ manager.py
@@ -22,23 +22,43 @@ Base.metadata.create_all(engine)
 
 
 def add_item():
+    """Adds item to Inventory based on parameters given to GUI"""
     display_window.delete(1.0, 'end')
     name = product_entry.get()
     price = price_entry.get()
     count = count_entry.get()
+    error = None
 
-    if name and price and count:
-        new_item = Item(name=name, price=price, count=count)
-        session.add(new_item)
-        session.commit()
+    if len(name) < 1:
+        error = 'name'
 
-    display_window.insert('end', f'{count} {name} at price ${price} added to inventory')
-    product_entry.delete(0, 'end')
-    price_entry.delete(0, 'end')
-    count_entry.delete(0, 'end')
+    try:
+        price = float(price)
+    except ValueError:
+        error = 'price'
+
+    try:
+        count = int(count)
+    except ValueError:
+        error = 'count'
+
+    if error is None:
+        if session.query(Item).filter(Item.name == name).first():
+            display_window.insert('end', f'{name} is already in inventory.\nDid you mean to update?')
+        else:
+            new_item = Item(name=name, price=price, count=count)
+            session.add(new_item)
+            session.commit()
+            display_window.insert('end', f'{count} {name} at price ${price} added to inventory')
+            product_entry.delete(0, 'end')
+            price_entry.delete(0, 'end')
+            count_entry.delete(0, 'end')
+    else:
+        display_window.insert('end', f'Enter valid {error} to add item to inventory')
 
 
 def update():
+    """Updates item or items in Inventory based on parameters given to GUI"""
     name = product_entry.get()
     price = price_entry.get()
     count = count_entry.get()
@@ -60,6 +80,7 @@ def display_inventory():
 
 
 def display_item():
+    """Displays item or items in Inventory based on parameters given to GUI"""
     display_window.delete(1.0, 'end')
     name = product_entry.get()
     price = price_entry.get()
@@ -148,6 +169,7 @@ def display_item():
 
 
 def delete_item():
+    """Deletes item in Inventory based on parameters given to GUI"""
     display_window.delete(1.0, 'end')
     name = product_entry.get()
     obj = session.query(Item).filter(Item.name == name).first()
@@ -172,6 +194,7 @@ entry_window.grid(row=0, column=0)
 tk.Label(entry_window, text='Item').grid(row=0, column=0, sticky='nsew')
 tk.Label(entry_window, text='Price').grid(row=0, column=1, sticky='nsew')
 tk.Label(entry_window, text='Count').grid(row=0, column=2, sticky='nsew')
+
 product_entry = tk.Entry(entry_window, width=40)
 product_entry.grid(row=1, column=0, sticky='nsew')
 price_entry = tk.Entry(entry_window, width=10)
@@ -179,8 +202,12 @@ price_entry.grid(row=1, column=1, sticky='nsew')
 count_entry = tk.Entry(entry_window, width=10)
 count_entry.grid(row=1, column=2, sticky='nsew')
 
-display_window = tk.Text(height=25, width=40)
+display_frame = tk.Canvas(root)
+display_frame.grid(row=1, column=1)
+display_window = tk.Text(display_frame, height=25, width=40, wrap='word')
 display_window.grid(row=1, column=1, sticky='nsew')
+scrollbar = tk.Scrollbar(display_frame, command=display_window.yview)
+scrollbar.grid(row=1, column=3, sticky='nsew')
 
 keypad = tk.Canvas()
 keypad.grid(row=1, column=0, sticky='nsew')
